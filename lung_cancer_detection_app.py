4bbe612adaac41a255ef6c2209776d3ce64bd0c5
Sangeeth Lal P S <pssangeethlal@gmail.com>
2021-05-18 22:18:29 +0530

    Repository updated

---
+++ lung_cancer_detection_app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+
+from keras.models import load_model
+st.set_option('deprecation.showfileUploaderEncoding',False)
+@st.cache(allow_output_mutation=True)
+def load_model1():
+    model = load_model('C:/Users/Dev-Laptop/Desktop/Project_Lung_cancer/best_model_1.hdf5')
+    return model
+model=load_model1()
+st.write("""
+        # Lung Cancer Detection
+         """)
+file = st.file_uploader("Upload the CT Image", type=["jpg","jpeg"])
+import numpy as np
+from keras.preprocessing import image
+from PIL import Image, ImageOps
+def import_and_predict(test_image, model):
+    size = (227,227)
+    test_image = ImageOps.fit(test_image, size, Image.ANTIALIAS)
+
+    test_image = image.img_to_array(test_image)
+    test_image = np.array([test_image], dtype=np.float16) / 255.0
+    prediction = model.predict(test_image)
+    return prediction
+if file is None:
+    st.text("Please upload an image file")
+else:
+    test_image = Image.open(file)
+    st.image(test_image, width= 400)
+    predictions = import_and_predict(test_image,model)
+    
+    categories = ["Cancerous","Non cancerous"]
+    string="The Uploaded CT image most likely to be "+categories[np.argmax(predictions)]
+    st.success(string)
+    
+    
+    
